d0fdd4721fef6053ae7b632f21cc0b186ba934f6
lstuma <lstuma@proton.me>
2023-10-27 11:48:21 +0200

    :sparkles:

--- src/protofire/http/http.py
+++ src/protofire/http/http.py
@@ -1,5 +1,4 @@
-import json
-import protofire.http.mime
+import protofire.http.mime as mime
 
 class HttpObject:
     def __init(self, headers):
@@ -10,6 +9,19 @@ class HttpObject:
             return None
         else:
             return self.HEADERS[key]
+
+    @classmethod
+    def _parse_headers(cls, headers_str):
+        header_lines = headers_str.split('\r\n')
+
+        # parse headers and header params and create 2 dicts from it
+        _headers = [_header.split(':', 1) for _header in header_lines[1:]]
+        headers = {_header[0].strip():_header[1].split(';')[0].strip() for _header in _headers}
+        header_params = {_header[0].strip():(_header[1].split(';'))[1:] for _header in _headers}
+        for header, params in header_params.items():
+            header_params[header] = {param[0].strip():param[1] for param in [_param.split('=', 1)+[True] for _param in params]}
+
+        return header_lines, headers, header_params
 
 
 class HttpRequest(HttpObject):
@@ -37,16 +49,24 @@ class HttpRequest(HttpObject):
             return self.GET[key]
 
     @classmethod
-    def from_str(cls, request: str):
+    def _from_str(cls, request: str):
         """
             Generates a valid HttpRequest object from a string representation of the request.
         """
-        header_lines = request[:request.index('\r\n\r\n')].split('\r\n')
+        headers_str, body_str = request.split('\r\n\r\n', 1)
+        header_lines, headers, header_params = JSONResponse._parse_headers(headers_str)
+
+        # split into method, resource and protocol
         method, resource, protocol = header_lines[0].split(' ')
+        # split resource locator into path and get params
         path, _get = resource.split('?')+['']
-        get = {param[0]:param[1] for param in [_param.split('=', 1) for _param in _get[1:].split('&')]}
-        headers = {header[0].strip():header[1].strip() for header in [_header.split(':', 1) for _header in header_lines[1:]]}
-        ...
+        # parse get params and create dict from it
+        if _get: get = {param[0]:param[1] for param in [_param.split('=', 1) for _param in _get[1:].split('&')]}
+        else: get = {}
+
+        if method != 'GET':
+            # retrieve parsed body
+            body = mime.parse_body(headers['Content-Type'], header_params, body_str)
 
 
 
