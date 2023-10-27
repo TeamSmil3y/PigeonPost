6ccf2eb9ad256c4afe520497abbc2f5fbe3bfa49
lstuma <lstuma@proton.me>
2023-10-27 10:29:11 +0200

    :bricks: :bug:

--- src/protofire/http/http.py
+++ src/protofire/http/http.py
@@ -10,6 +10,19 @@ class HttpObject:
         else:
             return self.HEADERS[key]
 
+    @classmethod
+    def _parse_headers(cls, headers_str):
+        header_lines = headers_str.split('\r\n')
+
+        # parse headers and header params and create 2 dicts from it
+        _headers = [_header.split(':', 1) for _header in header_lines[1:]]
+        headers = {_header[0].strip():_header[1].split(';')[0].strip() for _header in _headers}
+        header_params = {_header[0].strip():(_header[1].split(';'))[1:] for _header in _headers}
+        for header, params in header_params.items():
+            header_params[header] = {param[0].strip():param[1] for param in [_param.split('=', 1)+[True] for _param in params]}
+
+        return header_lines, headers, header_params
+
 
 class HttpRequest(HttpObject):
     def __init__(self, method: str, path: str, headers: dict = {}, get: str = '', post: str = '', put = ''):
@@ -41,7 +54,7 @@ class HttpRequest(HttpObject):
             Generates a valid HttpRequest object from a string representation of the request.
         """
         headers_str, body_str = request.split('\r\n\r\n', 1)
-        header_lines = request[:request.index('\r\n\r\n')].split('\r\n')
+        header_lines, headers, header_params = JSONResponse._parse_headers(headers_str)
 
         # split into method, resource and protocol
         method, resource, protocol = header_lines[0].split(' ')
@@ -50,13 +63,6 @@ class HttpRequest(HttpObject):
         # parse get params and create dict from it
         if _get: get = {param[0]:param[1] for param in [_param.split('=', 1) for _param in _get[1:].split('&')]}
         else: get = {}
-
-        # parse headers and header params and create 2 dicts from it
-        _headers = [_header.split(':', 1) for _header in header_lines[1:]]
-        headers = {_header[0].strip():_header[1].split(';')[0].strip() for _header in _headers}
-        header_params = {_header[0].strip():(_header[1].split(';'))[1:] for _header in _headers}
-        for header, params in header_params.items():
-            header_params[header] = {param[0].strip():param[1] for param in [_param.split('=', 1)+[True] for _param in params]}
 
         if method != 'GET':
             # retrieve parsed body
--- src/protofire/http/mime.py
+++ src/protofire/http/mime.py
@@ -1,24 +1,33 @@
+import protofire.http.http as http
 import json
 
 
 def parse_body(content_type: str, header_params: dict, body: str):
     if content_type in SUPPORTED_MIMETYPES:
         return SUPPORTED_MIMETYPES[content_type](header_params, body)
+    print('UNSUPPORTED MIME TYPE')
 
 
 def application_json(header_params, body: str):
     return json.loads(body)
 
+def form_data(header_params, body:str):
+    return {param[0]:param[1] for param in [_param.split('=', 1) for _param in body[1:].split('&')]}
+
 def multipart_form_data(header_params, body: str):
-    print(header_params['Content-Type'])
     if 'boundary' in header_params['Content-Type']:
         boundary = header_params['Content-Type']['boundary']
-        boundary = boundary[:body.index(boundary)]
         sections = [section.strip() for section in body.split(boundary)[1:-1]]
-        print(boundary, sections)
+        for section in sections:
+            header_str, body_str = section.split('\r\n\r\n')
+            header_lines, headers, header_params = http.HttpObject._parse_headers(header_str)
+            print('HEADERS: ', headers, header_params, body)
+            body = parse_body(headers['Content-Type'], headers, body_str)
+
 
 
 SUPPORTED_MIMETYPES = {
     'application/json': application_json,
     'multipart/form-data': multipart_form_data,
+    'form-data': form_data,
 }
