57a9dfaef5254076ce922b47489757d7cac8dca4
lstuma <lstuma@proton.me>
2023-10-28 11:32:14 +0200

    :bug:

--- src/protofire/core/handler.py
+++ src/protofire/core/handler.py
@@ -5,11 +5,16 @@ from protofire.http.http import HttpRequest, HttpResponse
 log = create_log('HANDLER', 'cyan')
 
 
-def handle_request(client_socket: socket.socket, client_address: tuple):
+def handle_request(client_sock: socket.socket, client_address: tuple):
     log(4, f'TREATING CONNECTION FROM {client_address[0]}:{client_address[1]} as HTTP request')
 
-    request = client_socket.recv(4096)
+    request = client_sock.recv(4096)
 
     #print(str(request, 'ascii').replace('\\', '\\\\').replace('\r', '\\r').replace('\n', '\\n'))
     http_request = HttpRequest._from_str(str(request, 'ascii'))
     log(4, f'RECEIVED REQUEST:\n{http_request}')
+    print('BODY:', http_request.BODY, '\nHEADERS:', http_request.HEADERS, '\nMETHOD:', http_request.method, '\nPATH:', http_request.path, '\nGET:', http_request.GET)
+
+    # close socket
+    client_sock.shutdown(socket.SHUT_RDWR)
+    client_sock.close()
