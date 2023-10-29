12cafc5690f6cc883babbdd96279cd890d678e7f
lstuma <lstuma@proton.me>
2023-10-29 20:00:05 +0100

    :truck:

--- src/protofire/standard/error_views.py
+++
@@ -1,6 +0,0 @@
-from protofire.http.http import  HttpResponse, HttpRequest, JSONResponse
-import protofire.http.status as status
-
-
-def std(request: HttpRequest, code: int):
-    return JSONResponse(data={'error':f'invalid request: {request.path}'}, status=status.get(code))
