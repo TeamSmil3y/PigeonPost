83edd9e516199c52faeb609cf0b0995f3fc98fcf
lstuma <lstuma@proton.me>
2023-10-30 02:11:13 +0100

    :wrench::truck:

--- src/protofire/conf/default.py
+++
@@ -1,7 +0,0 @@
-import protofire.http.mime as mime
-
-SUPPORTED_MIMETYPES = {
-    'application/json': mime.JSONParser,
-    'application/x-www-form-urlencoded': mime.UrlencodedFormParser,
-    'multipart/form-data': mime.MultiPartFormParser,
-}
