82442aa5d2aa4b000447074be556872ab61b2e67
lstuma <lstuma@proton.me>
2023-10-30 02:03:34 +0100

    :children_crossing:

--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,7 @@
-from protofire.http.http import JSONResponse
+from protofire.shortcuts import JSONResponse, render
 
 def welcome(request):
     return JSONResponse(data={'welcome':'Hello World!'})
+
+def counter(request):
+    return render('counter.html', context={'request': request})
