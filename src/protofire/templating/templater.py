d112ee89aefd6d681370d5891ea56c3084e5f10d
lstuma <lstuma@proton.me>
2023-10-30 02:02:51 +0100

    :sparkles: added templating

---
+++ src/protofire/templating/templater.py
@@ -0,0 +1,26 @@
+import protofire.conf.settings as _settings
+from protofire.http.http import HttpResponse
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import mimetypes
+import os
+
+env = None
+
+
+def load():
+    settings = _settings.get()
+    global env
+    env = Environment(
+        loader=FileSystemLoader(searchpath=settings.templates_dir),
+        autoescape=select_autoescape(),
+    )
+
+
+def render(template, context, status=200):
+    # get mimetype for file
+    settings = _settings.get()
+    mimetype = mimetypes.guess_type(settings.templates_dir / template)[0]
+    global env
+    rendered = env.get_template(template).render(**context)
+    return HttpResponse(headers={'Content-Type':mimetype}, data=rendered, status=status)
