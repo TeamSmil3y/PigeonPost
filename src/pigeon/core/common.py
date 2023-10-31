605ec4a02f6100d170df9ba0821a08f45f765ace
lstuma <lstuma@proton.me>
2023-10-31 16:44:35 +0100

    :memo:

--- src/pigeon/core/common.py
+++ src/pigeon/core/common.py
@@ -2,6 +2,9 @@ import asyncio
 
 
 def sync_to_async(sync_func):
+    """
+    Turns a sync function into its async counterpart.
+    """
     async def wrapper(*args, **kwargs):
         loop = asyncio.get_event_loop()
         return await loop.run_in_executor(None, sync_func, *args, **kwargs)
