7d0daac4b2a7173e442e41c08dd2899a8931e479
lstuma <lstuma@proton.me>
2023-11-01 12:56:12 +0100

    :truck: forgot to import date in components.__init__.py

--- src/pigeon/middleware/components/__init__.py
+++ src/pigeon/middleware/components/__init__.py
@@ -4,6 +4,7 @@ import pigeon.middleware.components.cors as cors
 import pigeon.middleware.components.method as method
 import pigeon.middleware.components.connection as connection
 import pigeon.middleware.components.server as server
+import pigeon.middleware.components.date as date
 
 #
 # Components used by middleware preprocessors and postprocessors to process requests.
