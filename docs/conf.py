5905dfbcddb5bc7e95c4aec46386365b0e1fb1e4
lstuma <lstuma@proton.me>
2023-11-05 00:23:38 +0100

    :memo:

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+html_css_files = ['css/style.css']
+html_theme_options = {
+    'logo_light': '_static/pigeon_round.png',
+    'logo_dark': '_static/pigeon_round.png',
+}
+html_favicon = '_static/pigeon_round.png'
+html_title = 'Pigeon Documentation'
