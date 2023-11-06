8844df4770fcef7ce6170dce773d90fda038d0cb
lstuma <g.lstuma@gmail.com>
2023-11-06 23:14:56 +0100

    :pencil2: removed accidental typo

--- src/pigeon/middleware/views.py
+++ src/pigeon/middleware/views.py
@@ -113,7 +113,7 @@ class ViewHandler:
     def get_func(self, path: str, mimetype: str) -> Callable | None:
         """
         Returns a decorated version (includes dynamic_params and auth) of the view for the requested path.
-        """GP
+        """
         view = self._get_view(path, mimetype)
         if not view:
             # no view found
