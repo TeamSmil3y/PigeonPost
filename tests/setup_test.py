bb431beafdcb9072a1aec167f95eee6ec6ee44e4
lstuma <g.lstuma@gmail.com>
2023-11-06 23:20:02 +0100

    :bug: fixed auth_handler missing in tests

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -1,6 +1,8 @@
 from pigeon.conf import Manager
 import pytest
 from pathlib import Path
+import pigeon.middleware.views as views
+import pigeon.middleware.auth as auth
 import tests.resources.settings as settings
 
 TESTS_DIR = Path(__file__).parent.resolve()
@@ -13,5 +15,12 @@ def restore():
     """
     # override default settings with settings for test environment
     Manager.override(settings)
+
+    # view handlers
+    Manager.view_handler = views.ViewHandler()
+    Manager.error_handler = views.ErrorHandler()
+    # auth handlers
+    Manager.auth_handler = auth.AuthHandler()
+
     # call manager.setup to configure runtime computed settings
     Manager._setup()
