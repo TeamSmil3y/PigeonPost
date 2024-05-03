012b342bf885736cc17c08172510c7472a41c5b7
lstuma <lstuma@proton.me>
2024-05-03 09:15:37 +0200

    :art: added dynamic params example to demo webapp

--- demo/other.py
+++ demo/other.py
@@ -10,6 +10,13 @@ def welcome(request):
     return HTTPResponse(data='Welcome! Hello World!', content_type='text/plain')
 
 
+@Pigeon.view('/user/{{param1}}/view')
+def api_view_user(request, dynamic_params):
+    if request.method == 'GET':
+        return f'<h1>You requested to view {dynamic_params.param1}!</h1>'
+    return HTTPResponse(data='method not allowed', status=405, content_type='text/plain')
+
+
 @Pigeon.view('/goodbye', 'application/json')
 def welcome(request):
     return {'cya': 'Bye World!'}
