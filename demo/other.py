01eaabe83fcabc977ae5ffccfe3092a911714aa7
lstuma <lstuma@proton.me>
2024-05-03 09:58:23 +0200

    :art: added view in demo project for cookies

--- demo/other.py
+++ demo/other.py
@@ -1,9 +1,14 @@
 from pigeon import Pigeon
 from pigeon.shortcuts import HTTPResponse
+import random
+import string
+import json
+
 
 @Pigeon.view('/welcome', 'application/json')
 def welcome(request):
     return {'welcome': 'Hello World!'}
+
 
 @Pigeon.view('/welcome', 'text/plain')
 def welcome(request):
@@ -15,6 +20,12 @@ def api_view_user(request, dynamic_params):
     if request.method == 'GET':
         return f'<h1>You requested to view {dynamic_params.param1}!</h1>'
     return HTTPResponse(data='method not allowed', status=405, content_type='text/plain')
+
+
+@Pigeon.view('/cookies', 'application/json')
+def cookies(request):
+    session_id=''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    return HTTPResponse(data=json.dumps(str(request.cookies) + ', Setting Cookie SESSION_ID='+session_id), content_type='application/json', cookies={"SESSION_ID": session_id})
 
 
 @Pigeon.view('/goodbye', 'application/json')
