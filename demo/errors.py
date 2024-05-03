602bbb69e8b9f8ba475bca4a2a06efacc1de171c
lstuma <lstuma@proton.me>
2024-05-03 09:14:38 +0200

    :bug: Fixed invalid json in demo error view

--- demo/errors.py
+++ demo/errors.py
@@ -1,7 +1,8 @@
 from pigeon import Pigeon
 from pigeon.http import HTTPResponse
+import json
 from pigeon.http.common import status
 
 @Pigeon.error(404)
 def not_found(request):
-    return HTTPResponse(status=404, data="{'error':'Could not find resource!'}", headers={'content-type':'application/json'})
+    return HTTPResponse(status=404, data=json.dumps({'error':'Could not find resource!'}), headers={'content-type':'application/json'})
